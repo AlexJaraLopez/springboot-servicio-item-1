cd166962e4ac3007233b78d6a40622457e7b0f3a
Alex Jara <alexjaralopez9@gmail.com>
2021-11-26 01:56:29 -0500

    archivo de ejemplo

---
+++ springboot-servicio-item-1/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItem1Application.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioItem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioItem1Application.class, args);
+	}
+
+}
