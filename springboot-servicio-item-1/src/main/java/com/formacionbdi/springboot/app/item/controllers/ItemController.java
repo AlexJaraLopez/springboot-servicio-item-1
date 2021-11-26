53a5b58a533a554984e87f5a45f664e887e03ecf
Alex Jara <alexjaralopez9@gmail.com>
2021-11-26 02:01:23 -0500

    archivo de ejemplo

---
+++ springboot-servicio-item-1/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,30 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+
+@RestController
+public class ItemController {
+	@Autowired
+	@Qualifier("serviceRestTemplate")
+	private ItemService itemservice;
+	
+	@GetMapping("/listar")
+	public List<Item> listar(){
+		return itemservice.findAll();
+	}
+	
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemservice.findById(id, cantidad);
+	}
+
+}
